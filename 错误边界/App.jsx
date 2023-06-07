7f48c13ae5ae03e1661290c98503c4f1664b346c
chenqi06 <chenqi06@rd.netease.com>
2023-06-07 17:35:41 +0800

    render

---
+++ 错误边界/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Context from "./components/Context";
+function App() {
+  return (
+    <div>
+      <Context></Context>
+    </div>
+  );
+}
+
+export default App;
